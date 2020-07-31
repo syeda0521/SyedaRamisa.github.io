39c5f8c5a7048d1fc01044cc32e41236962bd6e2
syeda0521 <40681829+syeda0521@users.noreply.github.com>
2020-07-31 21:24:05 +0800

    resume changes

---
+++ resumeData.js
@@ -0,0 +1,6 @@
+let resumeData = {
+  "imagebaseurl":"https://syedaramisa.github.io/",
+  "name": "Syeda Ramisa",
+  "role": "Frontend Developer"
+  }
+export default resumeData
--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Header from './components/header/header';
+import About from './components/about/about';
+import Resume from './components/resume/resume';
+import Portfolio from './components/portfolio/portfolio';
+
+import ContactUs from './components/contactus/contactus';
+import Footer from './components/footer/footer';
 import logo from './logo.svg';
 import './App.css';
 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <About />
+        <Resume />
+        <Portfolio />
+        
+        <ContactUs />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+/*
 function App() {
   return (
     <div className="App">
@@ -22,5 +46,7 @@ function App() {
     </div>
   );
 }
+
+*/
 
 export default App;
